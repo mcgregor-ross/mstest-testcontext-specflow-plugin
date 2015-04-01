db7c51d04f4d332bdc86179bd49c403975ad92c4
prosslmk <paul.ross@landmark.co.uk>
2015-04-01 16:12:08 +0100

    Inital Commit

---
+++ MsTestContext/CustomGeneratorProvider.cs
@@ -0,0 +1,64 @@
+﻿using System.CodeDom;
+using TechTalk.SpecFlow.Generator;
+using TechTalk.SpecFlow.Generator.UnitTestProvider;
+using TechTalk.SpecFlow.Utils;
+
+namespace MsTestContext
+{
+    public class CustomGeneratorProvider : MsTest2010GeneratorProvider
+    {
+
+        public CustomGeneratorProvider(CodeDomHelper codeDomHelper)
+            : base(codeDomHelper)
+        {
+        }
+
+        public override void SetTestClassInitializeMethod(TestClassGenerationContext generationContext)
+        {
+
+
+            // Adding TestContext as field
+
+            var field = new CodeMemberField()
+            {
+                Name = "testContext",
+                Type = new CodeTypeReference("Microsoft.VisualStudio.TestTools.UnitTesting.TestContext"),
+                Attributes = MemberAttributes.Private
+            };
+
+            // Adding TestContext properties
+
+            generationContext.TestClass.Members.Add(field);
+
+            var codeMemberProperty = new CodeMemberProperty();
+            codeMemberProperty.Name = "TestContext";
+            codeMemberProperty.Type = new CodeTypeReference("Microsoft.VisualStudio.TestTools.UnitTesting.TestContext");
+            codeMemberProperty.HasGet = true;
+            codeMemberProperty.HasSet = true;
+            codeMemberProperty.Attributes = MemberAttributes.Public;
+
+            // The getter
+            codeMemberProperty.GetStatements.Add(
+                new CodeMethodReturnStatement(
+                new CodeFieldReferenceExpression(
+                    new CodeThisReferenceExpression(), "testContext")));
+
+            // The setter
+            codeMemberProperty.SetStatements.Add(
+                new CodeAssignStatement(
+                    new CodeFieldReferenceExpression(
+                        new CodeThisReferenceExpression(), "testContext"), new CodePropertySetValueReferenceExpression()));
+
+            codeMemberProperty.SetStatements.Add(new CodeSnippetStatement("FeatureContext.Current[\"TestContext\"] = value;"));
+
+            generationContext.TestClass.Members.Add(codeMemberProperty);
+
+            generationContext.TestClass.BaseTypes.Add(new CodeTypeReference {BaseType = "FeatureBase"});
+            
+            base.SetTestClassInitializeMethod(generationContext);
+
+
+
+        }
+    }
+}
---
+++ MsTestContext/MsTestContextPlugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BoDi;
+using MsTestContext;
+using TechTalk.SpecFlow.Generator.Configuration;
+using TechTalk.SpecFlow.Generator.Plugins;
+using TechTalk.SpecFlow.Generator.UnitTestProvider;
+using TechTalk.SpecFlow.Infrastructure;
+
+[assembly: GeneratorPlugin(typeof(MsTestContextPlugin))]
+
+namespace MsTestContext
+{
+    public class MsTestContextPlugin : IGeneratorPlugin
+    {
+        public void RegisterDependencies(ObjectContainer container)
+        {
+        }
+
+        public void RegisterCustomizations(ObjectContainer container, SpecFlowProjectConfiguration generatorConfiguration)
+        {
+            container.RegisterTypeAs<CustomGeneratorProvider, IUnitTestGeneratorProvider>();
+        }
+
+        public void RegisterConfigurationDefaults(SpecFlowProjectConfiguration specFlowConfiguration)
+        {
+        }
+    }
+}
